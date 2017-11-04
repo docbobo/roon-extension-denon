fb65ad71da58416899acf37cb956372057639d63
boris@pruessmann.org <Boris Pruessmann>
2017-11-04 11:02:52 +0000

    Updated dependencies

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var denon = {};
 var roon = new RoonApi({
     extension_id:        'org.pruessmann.roon.denon',
     display_name:        'Denon/Marantz AVR',
-    display_version:     '0.0.3',
+    display_version:     '0.0.4',
     publisher:           'Doc Bobo',
     email:               'boris@pruessmann.org',
     website:             'https://github.com/docbobo/roon-extension-denon',
