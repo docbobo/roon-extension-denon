4296d5ad438a51f3e16eba4c3d6f2156f03af813
Jason Charrier <jason@innovativelanguage.com>
2021-10-25 14:05:16 -0500

    updated node-roon-api dependency. Fixes "this.moo.close is not a function" error.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var denon = {};
 var roon = new RoonApi({
     extension_id:        'org.pruessmann.roon.denon',
     display_name:        'Denon/Marantz AVR',
-    display_version:     '0.0.9',
+    display_version:     '0.0.10',
     publisher:           'Doc Bobo',
     email:               'boris@pruessmann.org',
     website:             'https://github.com/docbobo/roon-extension-denon',
