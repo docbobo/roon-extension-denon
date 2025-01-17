f8dd3216a07ba98a93e81d3a464b50ffc27976f4
Jason Charrier <jason@innovativelanguage.com>
2025-01-16 23:45:19 -0600

    removed absolute path to docker

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Building docker image..."
-/usr/local/bin/docker build . --file Dockerfile --tag docker.io/jcharr1/roon-extension-denon:latest
+docker build . --file Dockerfile --tag docker.io/jcharr1/roon-extension-denon:latest
 
 echo "Pushing to Docker Hub..."
-/usr/local/bin/docker push docker.io/jcharr1/roon-extension-denon:latest
+docker push docker.io/jcharr1/roon-extension-denon:latest
